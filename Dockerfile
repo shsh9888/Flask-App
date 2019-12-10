cf56467e43e4ca7a0ebd25a39719967a7625ba2e
Shravan Shetty <shsh9888@colorado.edu>
2019-12-09 21:31:12 -0700

    created a flask app

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu
+MAINTAINER Shravan <shsh9888@colorado.edu>
+RUN mkdir -p /srv
+WORKDIR /srv
+
+COPY entrypoint.sh /srv
+COPY rest-server.py /srv
+
+RUN chmod 777 /srv/rest-server.py
+RUN chmod 777 /srv/entrypoint.sh
+RUN apt-get update && apt-get install -y python3 python3-pip  && pip3 install cassandra-driver && pip3 install jsonpickle
+ENTRYPOINT ["/srv/entrypoint.sh"]
---
+++ rest-server.py
@@ -0,0 +1,74 @@
+##
+## Sample Flask REST server
+##
+
+from flask import Flask, request, Response
+import jsonpickle
+from cassandra.cluster import Cluster
+
+
+# Initialize the Flask application
+app = Flask(__name__)
+
+cluster = Cluster(['localhost'],port=9042)
+session = cluster.connect('iot')
+BYMIN="minutetelemetry"
+BYHOUR="hourlytelemetry"
+BYDAY="dailytelemetry"
+
+# if buildingName == 'All':
+# else:
+#     query = "SELECT buildingId FROM buildinginfo where buildingname={} ALLOW FILTERING".format(buildingName)
+#     buildings = session.execute(query)
+
+@app.route('/iot/aggregate/byminute/<buildingId>', methods=['GET'])
+def aggregateBymin(buildingId):
+    query = "SELECT deviceid, devicename FROM deviceinfo where buildingid={} ALLOW FILTERING".format(buildingId)
+    devices = session.execute(query)
+    responses =[]
+    for device in devices:
+        query = "SELECT * FROM {} where deviceid={} ALLOW FILTERING".format(BYMIN, device.deviceid)
+        print(query)
+        queryResponse = session.execute(query)
+        for response in queryResponse:
+            resObject = {"buildingId" : buildingId, "deviceId":device.deviceid,  "count" : response.count, "max" : response.max , "min" : response.min, "mean": response.mean}
+            responses.append(resObject)
+
+    return Response(response=jsonpickle.encode(responses), status=200, mimetype="application/json")
+
+@app.route('/iot/aggregate/byhour/<buildingId>', methods=['GET'])
+def aggregateByhour(buildingId):
+    query = "SELECT deviceid, devicename FROM deviceinfo where buildingid={} ALLOW FILTERING".format(buildingId)
+    devices = session.execute(query)
+    responses = []
+    for device in devices:
+        query = "SELECT * FROM {} where deviceid={} ALLOW FILTERING".format(BYHOUR, device.deviceid)
+        print(query)
+        queryResponse = session.execute(query)
+        for response in queryResponse:
+            resObject = {"buildingId": buildingId, "deviceId": device.deviceid, "count": response.count,
+                         "max": response.max, "min": response.min, "mean": response.mean}
+            responses.append(resObject)
+
+    return Response(response=jsonpickle.encode(responses), status=200, mimetype="application/json")
+
+
+@app.route('/iot/aggregate/byday/<buildingId>', methods=['GET'])
+def aggregateByDay(buildingId):
+    query = "SELECT deviceid, devicename FROM deviceinfo where buildingid={} ALLOW FILTERING".format(buildingId)
+    devices = session.execute(query)
+    responses =[]
+    for device in devices:
+        query = "SELECT * FROM {} where deviceid={} ALLOW FILTERING".format(BYDAY, device.deviceid)
+        print(query)
+        queryResponse = session.execute(query)
+        for response in queryResponse:
+            resObject = {"buildingId" : buildingId, "deviceId":device.deviceid,  "count" : response.count, "max" : response.max , "min" : response.min, "mean": response.mean}
+            responses.append(resObject)
+
+    return Response(response=jsonpickle.encode(responses), status=200, mimetype="application/json")
+
+# route http posts to this method
+
+# start flask app
+app.run(host="0.0.0.0", port=5000)
