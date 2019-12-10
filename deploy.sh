26ac1f65a7eb9aa75110506825a290fd23378774
Shravan Shetty <shsh9888@colorado.edu>
2019-12-09 23:33:07 -0700

    added the image version

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-kubectl create deployment restserver --image=gcr.io/lab7-258103//flaskapp:latest
+kubectl create deployment restserver --image=gcr.io/lab7-258103//flaskapp:1.0
 kubectl expose deployment restserver  --type=LoadBalancer --port 5000 --target-port 5000
